372cbaa255c273e78569e89f47584db6dbaac586
minzhang2 <1018644425@qq.com>
2018-12-03 16:09:52 +0800

    update promise

--- src/promise/es5-promise.js
+++ src/promise/es5-promise.js
@@ -1,25 +1,21 @@
 /**
  * promise => generator => async => core
  * 异步的相关原理
- * 
+ *
  * 1.promise: 指一个拥有符合规范的then方法的对象；
  * 2.thenable: 指一个定义了then方法的对象；
  * 3.决议（resolve）: 改变一个promise等待状态至已完成或被拒绝状态， 一旦决议，不再可变；
  * 4.值（value）: 一个任意合法的JavaScript值，包括undefined,thenable对象，promise对象；
  * 5.exception/error: JavaScript引擎抛出的异常/错误
  * 6.拒绝原因（reject reason）: 一个promise被拒绝的原因
- * 
+ *
  * 参考：http://es6.ruanyifeng.com/#docs/promise#Promise-resolve
  * 		https://github.com/xieranmaya/blog/issues/3
- * 		https://github.com/libin1991/webpack4-vue-more-page-cli/blob/master/A%E9%9D%A2%E8%AF%95%E4%BB%A3%E7%A0%81%E5%BA%93/js/Promise.js
  */
 
 const noop = () => {}
 const toString = {}.toString;
 
-/**
- * cb
- */
 function Promise(exec = noop) {
 	/**
 	 * pending => resolve(fulfilled, rejected)
@@ -29,8 +25,8 @@ function Promise(exec = noop) {
 	 */
 	this.status = 'pending';
 	this.value = null; // 存储数据
-	this.resolved = noop; // 收集then
-	this.rejected = noop; // 收集catch
+	this.resolveCb = noop; // 收集then
+	this.rejectCb = noop; // 收集catch
 
 	// 处理resolved情况
 	const resolve = (value) => {
@@ -38,8 +34,8 @@ function Promise(exec = noop) {
 			this.status = 'fulfilled';
 			this.value = value;
 			nextTick(() => {
-				this.resolved(value);
-			})
+        this.resolveCb(value)
+      })
 		}
 	}
 
@@ -49,8 +45,8 @@ function Promise(exec = noop) {
 			this.status = 'rejected';
 			this.value = value;
 			nextTick(() => {
-				this.rejected(value);
-			})
+        this.rejectCb(value)
+      })
 		}
 	}
 
@@ -62,47 +58,46 @@ function Promise(exec = noop) {
 
 }
 
-Promise.prototype.then = function (onResolved = noop, onRejected = noop) {
-	let p;
+Promise.prototype.then = function (thenResolve = noop, thenReject = noop) {
+	thenResolve = typeof thenResolve === 'function' ? thenResolve : value => value;
+	thenReject = typeof thenReject === 'function' ? thenReject : err => { throw err };
 
-	onResolved = typeof onResolved === 'function' ? onResolved : (value => value);
-	onRejected = typeof onRejected === 'function' ? onRejected : (err => { throw err });
-
-	const safeRun = function(thenCb, promiseCb, reject) {
-		try {
-			const x = thenCb(this.value)
-			if (x instanceof Promise) {
-				x.then(resolve, reject)
-			} else {
-				promiseCb(x)
-			}
-		} catch (err) {
-			reject(err);
-		}
+	const safeRun = function(thenResolveOrReject, resolve, reject, isResolveOrReject) {
+    var p = thenResolveOrReject(this.value);
+    try {
+      if(p instanceof Promise) {
+        p.then(resolve, reject);
+      } else {
+        const fn = isResolveOrReject ? resolve : reject;
+        fn(this.value);
+      }
+    } catch(err) {
+      reject(err);
+    }
 	}
 
 	if (this.status === 'pending') {
-		return p = new Promise((resolve, reject) => {
-			this.resolved = () => {
-				safeRun.call(this, onResolved, resolve, reject)
+		return new Promise((resolve, reject) => {
+			this.resolveCb = () => {
+				safeRun.call(this, thenResolve, resolve, reject, true);
 			};
-			this.rejected = () => {
-				safeRun.call(this, onRejected, reject, reject)
+			this.rejectCb = () => {
+        safeRun.call(this, thenReject, resolve, reject, false);
 			};
 		})
 	} else if (this.status === 'fulfilled') {
-		return p = new Promise((resolve, reject) => {
-			safeRun.call(this, onResolved, resolve, reject)
+		return new Promise((resolve, reject) => {
+			safeRun.call(this, thenResolve, resolve, reject, true);
 		});
 	} else if (this.status === 'rejected') {
-		return p = new Promise((resolve, reject) => {
-			safeRun.call(this, onRejected, reject, reject)
+		return new Promise((resolve, reject) => {
+			safeRun.call(this, thenReject, resolve, reject, false);
 		});
 	}
 }
 
-Promise.prototype.catch = function(onRejected) {
-	return this.then(null, onRejected);
+Promise.prototype.catch = function(reject) {
+	return this.then(null, reject);
 }
 
 /**
@@ -124,7 +119,7 @@ Promise.resolve = function(value) {
 }
 
 Promise.reject = function(err) {
-	return new Promise((resolve, reject) => {
+	return new Promise((_, reject) => {
 		reject(err)
 	})
 };
@@ -136,22 +131,24 @@ Promise.reject = function(err) {
  * 2.只要p1、p2、p3之中有一个被rejected，p的状态就变成rejected，此时第一个被reject的实例的返回值，会传递给p的回调函数。
 */
 Promise.all = function(promises) {
-	const ret = [];
 	return new Promise((resolve, reject) => {
-		let lastIndex = promises.length - 1;
-		let index = 0;
-		while(index <= lastIndex) {
-			Promise.resolve(promises[index]).then(res => {
-				ret.push(res)
-				if(index === lastIndex) {
-					return resolve(ret);
-				}
-			}, err => {
-				return reject(err);
-			});
-			index++;
-		}
-	})
+    const ret = [];
+    let i = 0;
+    const next = () => {
+      Promise.resolve(promises[i]).then(res => {
+        ret.push(res);
+        i++;
+        if(i === promises.length) {
+          resolve(ret);
+        } else {
+          next();
+        }
+      }, err => {
+        return reject(err);
+      });
+    }
+    next();
+  });
 }
 
 /**
@@ -159,18 +156,13 @@ Promise.all = function(promises) {
  * 只要p1、p2、p3之中有一个实例率先改变状态，p的状态就跟着改变。那个率先改变的 Promise 实例的返回值，就传递给p的回调函数。
 */
 Promise.race = function(promises) {
-	return new Promise((resolve, reject) => {
-		let lastIndex = promises.length - 1;
-		let index = 0;
-		while(index <= lastIndex) {
-			promises[index].then(res => {
-				return resolve(res);
-			}, err => {
-				return reject(err);
-			});
-			index++;
-		}
-	})
+  return new Promise((resolve, reject) => {
+    Promise.resolve(promises[0]).then(res => {
+      resolve(res);
+    }, err => {
+      reject(err);
+    });
+  });
 }
 
 // 参考immediate的顺序
--- src/promise/index.js
+++ src/promise/index.js
@@ -1,11 +1,13 @@
-import  Promise from './Promise';
+// import  Promise from './es5-promise';
+// import  Promise from './es6-promise';
 // import Promise from './lie';
 
-window.pro = new Promise(function (resolve, reject) {
-	console.log('p')
-	setTimeout(() => {
-		resolve(2)
-	}, 1000)
-})/*.then(res => {
-	return new Promise(resolve => setTimeout(() => resolve(res), 1000));
-}) */.then(res => res).then(res => console.log(res))
+
+// window.pro = new Promise(function (resolve, reject) {
+// 	console.log('p')
+// 	setTimeout(() => {
+// 		resolve(2)
+// 	}, 1000)
+// })/*.then(res => {
+// 	return new Promise(resolve => setTimeout(() => resolve(res), 1000));
+// }) */.then(res => res).then(res => console.log(res))
